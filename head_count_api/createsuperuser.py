d3a19a05ad2c263779af5513888a2fc8c0f5228d
Subhash Amale <subhash.amale@expersistech.com>
2019-01-18 16:00:33 +0530

    Modified super from Manoj to Aparna

--- head_count_api/createsuperuser.py
+++ head_count_api/createsuperuser.py
@@ -9,11 +9,12 @@ from django.contrib.auth.models import User
 try:
 
     try:
-        user = User.objects.get(username='manoj.patwardhan@xebrium.com')
+        user = User.objects.get(username='aparna.ket@xebrium.com')
     except User.DoesNotExist as e:
-        user = User.objects.create_user('manoj.patwardhan@xebrium.com', 'manoj.patwardhan@xebrium.com', 'xebrium@2018')
+        user = User.objects.create_user('aparna.ket@xebrium.com', 'aparna.ket@xebrium.com', 'xebrium@2018')
         user.is_superuser = True
-        user.first_name = "Administrator"
+        user.first_name = "Aparna"
+        user.last_name = "Ket"
         user.is_staff = True
         user.save()
 except Exception as e:
